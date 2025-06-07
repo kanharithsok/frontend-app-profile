bfb9a4be9d7427687b6ce266f89a7104d464e5f9
ReaksmeyThan <reaksmey.than7up@gmail.com>
2025-06-07 12:49:30 +0700

    add translate

---
+++ src/Footer/messages.js
@@ -0,0 +1,31 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  myCourse: {
+    id: 'myCourse',
+    description: 'My Course link in footer',
+    defaultMessage: 'My Course',
+  },
+  library: {
+    id: 'learnerVariantDashboard.library',
+    description: 'Library link in footer',
+    defaultMessage: 'Library',
+  },
+  contactUs: {
+    id: 'learnerVariantDashboard.contactUs',
+    description: 'Contact Us link in footer',
+    defaultMessage: 'Contact Us',
+  },
+  account: {
+    id: 'learnerVariantDashboard.account',
+    description: 'Account link in footer',
+    defaultMessage: 'Account',
+  },
+  allRightsReserved: {
+    id: 'learnerVariantDashboard.allRightsReserved',
+    description: 'All rights reserved message in footer',
+    defaultMessage: '© 2025 Credit Bureau Cambodia. All rights reserved.',
+  },
+});
+
+export default messages;
